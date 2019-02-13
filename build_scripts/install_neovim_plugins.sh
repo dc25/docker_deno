b45fe26ad41e92f2bc1cd7d56d10fe47d048e1c3
Dave Compton <davecompton7@gmail.com>
2019-02-13 12:27:46 -0800

    adding filedetect override for ts files.

--- build_scripts/install_neovim_plugins.sh
+++ build_scripts/install_neovim_plugins.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
 nvim +PlugInstall +UpdateRemotePlugins +qa
+mkdir -p .config/nvim/ftdetect
+
+cat > .config/nvim/ftdetect/ts.vim << EOF
+au BufRead,BufNewFile *.ts              set filetype=typescript   
+EOF
